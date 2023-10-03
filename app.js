1214fde5918ffd4766fe6601bfffe411e3b6b830
EDMerstratec <erme0001@edu.mercantec.dk>
2023-10-03 17:58:46 +0200

    Opgave 1.2 til 1.3

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ console.log(
 );
 //Lav jeres svar her
 console.log("Hello World");
+
+console.log("Opgave 1.2 - Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen")
+//either two commands, or \n for newline
+console.log("Hello\nWorld")
+//console.log("Hello")
+//console.log("World")
+
+console.log("Opgave 1.3 - Lav et program, som skriver følgende i konsollen.")
+//Similar. Repeat logs or \n newlines
+console.log("*\n**\n***\n**\n*")
+//console.log("*")
+//console.log("**")
+//console.log("***")
+//console.log("**")
+//console.log("*")
+
